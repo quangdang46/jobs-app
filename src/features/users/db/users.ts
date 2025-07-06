a23d601e8ae945dd2321d7605cba742ff03fb408
Tran Quang Dang <tranquangdang21@gmail.com>
2025-07-06 23:07:47 +0700

    feat: Enable experimental caching in Next.js configuration and enhance user data handling. Added cache revalidation for user operations and implemented caching in getUser function for improved performance.

--- src/features/users/db/users.ts
+++ src/features/users/db/users.ts
@@ -1,9 +1,11 @@
 import { db } from "@/drizzle/db";
 import { UserTable } from "@/drizzle/schema";
+import { revalidateUserCache } from "@/features/users/db/cache/users";
 import { eq } from "drizzle-orm";
 
 export async function insertUser(user: typeof UserTable.$inferInsert) {
   await db.insert(UserTable).values(user).onConflictDoNothing();
+  revalidateUserCache(user.id);
 }
 
 export async function updateUser(
@@ -11,8 +13,10 @@ export async function updateUser(
   user: Partial<typeof UserTable.$inferInsert>
 ) {
   await db.update(UserTable).set(user).where(eq(UserTable.id, id));
+  revalidateUserCache(id);
 }
 
 export async function deleteUser(id: string) {
   await db.delete(UserTable).where(eq(UserTable.id, id));
+  revalidateUserCache(id);
 }
