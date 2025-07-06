16aee7882c14ddc6dcedef44cc4f1b2f8042c0b2
Tran Quang Dang <tranquangdang21@gmail.com>
2025-07-06 17:28:15 +0700

    feat: Integrate Clerk webhook functionality and update environment variables. Added Clerk user management functions to Inngest API routes, included new webhook secret in environment configuration, and updated package.json with the latest Svix dependency.

--- src/services/inngest/client.ts
+++ src/services/inngest/client.ts
@@ -1,4 +1,22 @@
-import { Inngest } from "inngest";
+import { DeletedObjectJSON, UserJSON } from "@clerk/nextjs/server";
+import { EventSchemas, Inngest } from "inngest";
+
+type ClerkWebhookData<T> = {
+  data: {
+    data: T;
+    raw: string;
+    headers: Record<string, string>;
+  };
+};
+
+type Events = {
+  "clerk/user.created": ClerkWebhookData<UserJSON>;
+  "clerk/user.updated": ClerkWebhookData<UserJSON>;
+  "clerk/user.deleted": ClerkWebhookData<DeletedObjectJSON>;
+};
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "jobs-app-inngest" });
+export const inngest = new Inngest({
+  id: "jobs-app-inngest",
+  schemas: new EventSchemas().fromRecord<Events>(),
+});
---
+++ src/services/inngest/functions/clerk.ts
@@ -0,0 +1,117 @@
+import { env } from "@/data/env/server";
+import { insertUserNotificationSettings } from "@/features/users/db/userNotificationSettings";
+import { deleteUser, insertUser, updateUser } from "@/features/users/db/users";
+import { inngest } from "@/services/inngest/client";
+import { NonRetriableError } from "inngest";
+import { Webhook } from "svix";
+
+function verifyWebhook({
+  raw,
+  headers,
+}: {
+  raw: string;
+  headers: Record<string, string>;
+}) {
+  return new Webhook(env.CLERK_WEBHOOK_SECRET).verify(raw, headers);
+}
+
+export const clerkCreateUser = inngest.createFunction(
+  {
+    id: "clerk/create-db-user",
+    name: "Clerk Create User",
+  },
+  {
+    event: "clerk/user.created",
+  },
+  async ({ event, step }) => {
+    await step.run("verify-webhook", async () => {
+      try {
+        verifyWebhook(event.data);
+      } catch (error) {
+        throw new NonRetriableError("Maximum times retries");
+      }
+    });
+
+    const userId = await step.run("create-user", async () => {
+      const userData = event.data.data;
+
+      const email = userData.email_addresses.find(
+        (email) => email.id === userData.primary_email_address_id
+      );
+
+      if (email == null) {
+        throw new NonRetriableError("No email address found");
+      }
+
+      await insertUser({
+        id: userData.id,
+        name: `${userData.first_name} ${userData.last_name}`,
+        email: email.email_address,
+        imageUrl: userData.image_url,
+        createdAt: new Date(userData.created_at),
+        updatedAt: new Date(userData.updated_at),
+      });
+
+      return userData.id;
+    });
+
+    await step.run("create-user-notification-settings", async () => {
+      await insertUserNotificationSettings({ userId });
+    });
+  }
+);
+
+export const clerkUpdateUser = inngest.createFunction(
+  { id: "clerk/update-db-user", name: "Clerk - Update DB User" },
+  { event: "clerk/user.updated" },
+  async ({ event, step }) => {
+    await step.run("verify-webhook", async () => {
+      try {
+        verifyWebhook(event.data);
+      } catch {
+        throw new NonRetriableError("Invalid webhook");
+      }
+    });
+
+    await step.run("update-user", async () => {
+      const userData = event.data.data;
+      const email = userData.email_addresses.find(
+        (email) => email.id === userData.primary_email_address_id
+      );
+
+      if (email == null) {
+        throw new NonRetriableError("No primary email address found");
+      }
+
+      await updateUser(userData.id, {
+        name: `${userData.first_name} ${userData.last_name}`,
+        imageUrl: userData.image_url,
+        email: email.email_address,
+        updatedAt: new Date(userData.updated_at),
+      });
+    });
+  }
+);
+
+export const clerkDeleteUser = inngest.createFunction(
+  { id: "clerk/delete-db-user", name: "Clerk - Delete DB User" },
+  { event: "clerk/user.deleted" },
+  async ({ event, step }) => {
+    await step.run("verify-webhook", async () => {
+      try {
+        verifyWebhook(event.data);
+      } catch {
+        throw new NonRetriableError("Invalid webhook");
+      }
+    });
+
+    await step.run("delete-user", async () => {
+      const { id } = event.data.data;
+
+      if (id == null) {
+        throw new NonRetriableError("No id found");
+      }
+      await deleteUser(id);
+    });
+  }
+);
